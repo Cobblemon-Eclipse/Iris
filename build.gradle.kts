b10bcf5d6a64206246d4b2b185d891f386aa3192
IMS212 <ims@irisshaders.dev>
2025-02-19 18:01:10 -0800

    Source compatible with latest Neo

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 val MINECRAFT_VERSION by extra { "1.21.4" }
-val NEOFORGE_VERSION by extra { "21.4.13-beta" }
+val NEOFORGE_VERSION by extra { "21.4.92-beta" }
 val FABRIC_LOADER_VERSION by extra { "0.16.9" }
 val FABRIC_API_VERSION by extra { "0.110.5+1.21.4" }
 
